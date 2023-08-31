70361cf0700d4cb6370c30098460f3d4b796f7b5
PawelNorek <122644701+PawelNorek@users.noreply.github.com>
2023-08-31 21:29:25 +0200

    Create chat.mjs

---
+++ chat.mjs
@@ -0,0 +1,28 @@
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+// You cannot use directly __dirname in module - you need following
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+//and additional two lines
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(__dirname + '/public'))
+
+io.on('connection', socket => {
+	console.log(socket.id, ' has connected')
+	// socket.emit('messageFromServer', { data: 'Welcome from socket server!' })
+	socket.on('newMessageToServer', dataFromClient => {
+		console.log(dataFromClient.text)
+		io.emit('newMessageToClients', { text: dataFromClient.text })
+	})
+})
+
+httpServer.listen(3001)
